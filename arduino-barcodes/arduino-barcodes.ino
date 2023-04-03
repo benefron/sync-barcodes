5c0c7f15c8be37cfaee4089cfa95c6dd3d34db56
benefron <benefron@gmail.com>
2023-04-03 08:42:40 +0300

    added control by serial

--- arduino-barcodes/arduino-barcodes.ino
+++ arduino-barcodes/arduino-barcodes.ino
@@ -5,39 +5,50 @@
 int OUTPUT_PIN = 13;
 int BARCODE_BITS = 32;
 int INTER_BARCODE_INTERVAL = 30; // s
+// create an empty char variable
+char Control;
 
 long barcode;
 
-void setup() {
-  
-  pinMode(OUTPUT_PIN, OUTPUT); // initialize digital pin
+void setup()
+{
+
+  pinMode(OUTPUT_PIN, OUTPUT);          // initialize digital pin
+  Serial.begin(9600);                   // initialize serial communication
 
   randomSeed(analogRead(0));
 
-  barcode = random(0, pow(2,BARCODE_BITS));
-  
+  barcode = random(0, pow(2, BARCODE_BITS));
 }
 
-void loop() {
-
-  barcode += 1; // increment barcode on each cycle
-
-  digitalWrite(OUTPUT_PIN, HIGH);   // initialize with 20 ms pulse
-  delay(20);
-  digitalWrite(OUTPUT_PIN, LOW);    // set to low value for 20 ms
-  delay(20);
-
-  for (int i = 0; i < BARCODE_BITS; i++)
+void loop()
+{
+  if (Serial.available())
   {
-    if ((barcode >> i) & 1)
-      digitalWrite(OUTPUT_PIN,HIGH);
-    else
-      digitalWrite(OUTPUT_PIN,LOW);
-
-     delay((INTER_BARCODE_INTERVAL - 1) * 32 / BARCODE_BITS);
+    Control = Serial.read();
   }
-  
-  digitalWrite(OUTPUT_PIN, LOW);         // write final low value
-  delay(INTER_BARCODE_INTERVAL * 1000);  // wait for interval
 
+  if (Control == 's')
+  {
+
+    barcode += 1; // increment barcode on each cycle
+
+    digitalWrite(OUTPUT_PIN, HIGH); // initialize with 20 ms pulse
+    delay(20);
+    digitalWrite(OUTPUT_PIN, LOW); // set to low value for 20 ms
+    delay(20);
+
+    for (int i = 0; i < BARCODE_BITS; i++)
+    {
+      if ((barcode >> i) & 1)
+        digitalWrite(OUTPUT_PIN, HIGH);
+      else
+        digitalWrite(OUTPUT_PIN, LOW);
+
+      delay((INTER_BARCODE_INTERVAL - 1) * 32 / BARCODE_BITS);
+    }
+
+    digitalWrite(OUTPUT_PIN, LOW);        // write final low value
+    delay(INTER_BARCODE_INTERVAL * 1000); // wait for interval
+  }
 }
